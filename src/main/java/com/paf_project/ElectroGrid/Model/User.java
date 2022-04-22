649acbc9bd8b44c12c0ba80470897b4a8789c2c8
sasanka <it20037956@my.sliit.lk>
2022-04-22 19:45:07 +0530

    user management model created

---
+++ src/main/java/com/paf_project/ElectroGrid/Model/User.java
@@ -0,0 +1,63 @@
+package com.paf_project.ElectroGrid.Model;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+
+	private String userId;
+	private String name;
+	private String nic;
+	private String address;
+	private int phone;
+	private String email;
+	private String password;
+	
+	
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNic() {
+		return nic;
+	}
+	public void setNic(String nic) {
+		this.nic = nic;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
--- src/main/java/com/paf_project/ElectroGrid/UserResource.java
+++ src/main/java/com/paf_project/ElectroGrid/UserResource.java
@@ -1,5 +1,7 @@
 package com.paf_project.ElectroGrid;
 
+import com.paf_project.ElectroGrid.Model.User;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -7,10 +9,21 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("user")
 public class UserResource {
-	  @GET
-	    @Produces(MediaType.TEXT_PLAIN)
-	    public String getIt() {
-	        return "Got it!";
+	  
+		@GET
+	    @Produces(MediaType.APPLICATION_JSON)
+	    public User getUser() {
+		  		  
+		  User user = new User();
+		  user.setUserId("1");
+		  user.setName("Amal");
+		  user.setNic("123");
+		  user.setAddress("Malabe");
+		  user.setPhone(1234);
+		  user.setEmail("amal@gmail.com");
+		  user.setPassword("123");
+		  return user;
+	        
 	    }
 	
 }
